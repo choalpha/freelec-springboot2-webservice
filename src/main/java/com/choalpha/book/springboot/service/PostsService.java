cc6da1da5108335700c9567999cacc878767b864
hoon <1bun0403@gmail.com>
2021-10-20 02:05:20 +0900

    등록 API 추가

---
+++ src/main/java/com/choalpha/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.choalpha.book.springboot.service;
+
+import com.choalpha.book.springboot.domain.posts.PostsRepository;
+import com.choalpha.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
