b0adc7283ab5a4e16bab61e9d7bc1c380c5740bd
hoon <1bun0403@gmail.com>
2021-10-31 18:34:54 +0900

    수정/조회 API 추가

--- src/test/java/com/choalpha/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/choalpha/book/springboot/web/PostsApiControllerTest.java
@@ -69,10 +69,10 @@ public class PostsApiControllerTest {
     public void Posts_수정된다() throws Exception {
         // given
         Posts savedPosts = postsRepository.save(Posts.builder()
-        .title("title")
-        .content("content")
-        .author("author")
-        .build());
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
 
         Long updateId = savedPosts.getId();
         String expectedTitle = "title2";
